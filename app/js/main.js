bdde2666a481abc6a733d1ced26779f80d1c3bd6
Rogov <rogov.veniamin@gmail.com>
2021-02-10 20:15:15 +0700

    2

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,8 @@
 $(function(){
 
   $('.photo__slider').slick({
-    // prevArrow: '<button type="button" class="slick-prev"><img src="./img/svg/arrow-left.svg" alt="стрелка влево"></button>',
-    // nextArrow: '<button type="button" class="slick-next"><img src="./img/svg/arrow-right.svg" alt="стрелка вправо"></button>'
+    prevArrow: '<button type="button" class="slick-prev"><img src="/img/svg/arrow-left.svg" alt="стрелка влево"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="/img/svg/arrow-right.svg" alt="стрелка вправо"></button>'
   });
 
 });
